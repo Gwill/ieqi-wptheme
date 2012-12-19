401e2ad1afe1e95ab02f9c27f84d48cd3224673f
G_will <g_will@ieqi.com>
2012-12-19 18:31:52 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ $.SyntaxHighlighter.init({
     'prettifyBaseUrl': 'http://ieqi.net/wp-content/themes/ieqi-wptheme/jquery-syntaxhighlighter/prettify',
     'baseUrl': 'http://ieqi.net/wp-content/themes/ieqi-wptheme/jquery-syntaxhighlighter'
 });
+$(function(){
+    $("pre").animate({
+        opacity:1
+    }, 500);
+});
