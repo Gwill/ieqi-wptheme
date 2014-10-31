4e6e8c3be7b8c90b8ab77c9c45b6f10fc63a1d3a
G_will <g_will@ieqi.com>
2014-10-31 12:37:12 +0800

    fix jquery load

--- index.php
+++ index.php
@@ -32,8 +32,7 @@
             </div>
         </section>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.8.3.min.js"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.syntaxhighlighter.min.js"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
